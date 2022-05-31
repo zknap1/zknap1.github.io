a0a35eced24b5ab59eba1157889d1e9bd9b16603
zknap1 <73251034+zknap1@users.noreply.github.com>
2022-05-31 18:23:23 +0200

    Format code

--- sax_data_download.py
+++ sax_data_download.py
@@ -15,11 +15,9 @@ except Exception as e:
 
 try:
     chrome_webdriver_path = config_dict['chrome_webdriver_path']
-    downloads=config_dict['downloads']
+    downloads = config_dict['downloads']
 except Exception as e:
     print("Failed to load: "+str(e))
-
-
 
 
 options = webdriver.ChromeOptions()
@@ -40,16 +38,14 @@ URL = "http://www.bsse.sk/bcpben/Trading/Indices/SAXIndex/tabid/163/language/en-
 driver.get(URL)
 
 # download excel file
-export_data_xpath='/html/body/form/dllimport/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td[2]/table/tbody/tr[5]/td/table/tbody/tr[1]/td[1]/div[1]/div/div/table[2]/tbody/tr[2]/td[2]/div[2]/a'
+export_data_xpath = '/html/body/form/dllimport/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td[2]/table/tbody/tr[5]/td/table/tbody/tr[1]/td[1]/div[1]/div/div/table[2]/tbody/tr[2]/td[2]/div[2]/a'
 driver.find_element_by_xpath(export_data_xpath).click()
 
 time.sleep(2)
-df=pd.read_csv(downloads+"SAXIndex.csv")
+df = pd.read_csv(downloads+"SAXIndex.csv")
 print(df)
 
 if os.path.exists(downloads+"SAXIndex.csv"):
     os.remove(downloads+"SAXIndex.csv")
 
 driver.close()
-
-
